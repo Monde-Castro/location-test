16c0aef1490b2060b30a264f87752d47e9b60fed
Monde Modia <castromonde9@gmail.com>
2019-10-28 15:32:02 +0200

    Refactor show.ejs styles.css

--- models/review.js
+++ models/review.js
@@ -9,10 +9,6 @@ author:
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
-    image: {
-		secure_url: { type: String, default: '/images/default-profile.jpg' },
-		public_id: String
-	}
 });
 
 
