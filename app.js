89acada7dff44dc9f0f9b0c37d35f5594c21865c
Monde Modia <castromonde9@gmail.com>
2019-11-02 01:08:10 +0200

    Monde Modiba

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 // connect to the database
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/Location', { 
+mongoose.connect(process.env.MONGODB_URI , { 
 
   useNewUrlParser: true,
   useCreateIndex: true,
