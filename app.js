384870b6a58873b67151c6f5ea2c660f3b45a51c
Monde Modia <castromonde9@gmail.com>
2019-11-23 10:35:12 +0200

    Initial commit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const reviewsRouter = require('./routes/reviews');
 
 const app = express();
 // connect to the database
-mongoose.connect( process.env.MONGODB_URI || 'mongodb://localhost:27017/Location', { 
+mongoose.connect('mongodb://localhost:27017/Location', { 
 
   useNewUrlParser: true,
   useCreateIndex: true,
